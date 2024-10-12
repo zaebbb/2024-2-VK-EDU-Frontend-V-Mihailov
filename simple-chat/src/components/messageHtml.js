2e01d9463822f35c6cb7c012849c5d6c1146c0d0
zaebbb <elik@example.com>
2024-10-12 16:07:37 +0500

    Добавление анимаций - dz4

--- simple-chat/src/components/messageHtml.js
+++ simple-chat/src/components/messageHtml.js
@@ -3,9 +3,10 @@ import { USER } from './constants'
 const messageTemplate = (messageInfo) => {
   const messageElement = document.createElement('div');
   messageElement.classList.add('message');
+  messageElement.classList.add(messageInfo.user !== USER ? 'message--participant' : 'message--user');
   
   messageElement.innerHTML = `
-    <p class="message__content ${messageInfo.user !== USER ? 'message--participant' : ''}">
+    <p class="message__content">
       ${messageInfo.message}
     </p>
     <span class="message__time">${messageInfo.time}</span>
@@ -21,4 +22,8 @@ export const messageHtml = (
   const messageElement = messageTemplate(messageInfo)
   messagesContainer.appendChild(messageElement);
   messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+  requestAnimationFrame(() => {
+    messageElement.classList.add('show');
+  })
 }
